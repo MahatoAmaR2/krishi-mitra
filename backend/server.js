1cd6eafdb00bd715b548a06c72583df2cd7804ef
MahatoAmaR2 <bauaa476@gmail.com>
2026-05-06 20:26:47 +0530

    Database connected

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,24 @@
 import dotenv from "dotenv";
 import { app } from "./src/app.js";
+import dns from "node:dns"
+import connectDB from "./src/config/connectDB.js";
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 dotenv.config({
   path: "./.env",
 });
 
+
+
 const PORT = process.env.PORT || 3232;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+  });
